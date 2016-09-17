1a5e8f872b9942e3e96d79b8b0778a19c6bfb7ee
Nathan Myers <nathanemyers@gmail.com>
2016-09-16 21:33:11 -0500

    fleshing out week_data

--- nba/scrapper/week_data.py
+++ nba/scrapper/week_data.py
@@ -1,22 +1,36 @@
+import json
 from nba.models import Ranking
 
 class WeekData:
-
-    self.rankings = []
+    
+    def __init__(self, year, week):
+        self.rankings = []
+        self.year = year
+        self.week = week
 
     def add_rank(self, data):
         self.rankings.append(data)
-
-    def to_json(self):
-        return
-
-    def load_from_json(self, json):
-        return
+        
 
     def save(self):
-        verify_length()
-        verify_ranks()
-        return
+            
+        if not verify_length():
+            raise ValueError('Missing Teams!')
+        if not verify_ranks():
+            raise ValueError('Ranks are not uniform!')
+
+        self.rankings = map(lambda x: process_rank(x, self.year, self.week), self.rankings)
+
+        for rank in self.rankings:
+            rank_object = Ranking(
+                    year = self.year,
+                    rank = data.rank,
+                    record = data.record,
+                    team = team,
+                    summary = data.comment_string,
+                    week = data.week
+                    )
+            rank_object.save()
 
     def verify_length(self):
         if self.rankings.__len__ != 30:
@@ -25,4 +39,93 @@ class WeekData:
 
     def verify_ranks(self):
         return True
+
+    def to_json(self):
+        json.dumps(self.rankings)
+        return
+
+    def load_from_json(self, json):
+        return
+
+def process_rank(rank, year, week):
+    rank.team = resolve_team(rank.team)
+    rank.summary = stripTags(rank.summary)
+    rank.year = year
+    rank.week = week
+    return rank
+
+def stripTags(html, invalid_tags):
+    for tag in html:
+        if tag.name in invalid_tags:
+            s = ""
+            for c in tag.contents:
+                if not isinstance(c, NavigableString):
+                    c = stripTags(unicode(c), invalid_tags)
+                s += unicode(c)
+
+            tag.replaceWith(s)
+    return html
+
+def resolve_team(team_html_link):
+    if 'lakers' in team_html_link:
+        return Team.objects.get(name='Lakers')
+    if 'clippers' in team_html_link:
+        return Team.objects.get(name='Clippers')
+    if 'warriors' in team_html_link:
+        return Team.objects.get(name='Warriors')
+    if 'cavaliers' in team_html_link:
+        return Team.objects.get(name='Cavaliers')
+    if 'spurs' in team_html_link:
+        return Team.objects.get(name='Spurs')
+    if 'thunder' in team_html_link:
+        return Team.objects.get(name='Thunder')
+    if 'rockets' in team_html_link:
+        return Team.objects.get(name='Rockets')
+    if 'grizzlies' in team_html_link:
+        return Team.objects.get(name='Grizzlies')
+    if 'hawks' in team_html_link:
+        return Team.objects.get(name='Hawks')
+    if 'heat' in team_html_link:
+        return Team.objects.get(name='Heat')
+    if 'bulls' in team_html_link:
+        return Team.objects.get(name='Bulls')
+    if 'pelicans' in team_html_link:
+        return Team.objects.get(name='Pelicans')
+    if 'raptors' in team_html_link:
+        return Team.objects.get(name='Raptors')
+    if 'celtics' in team_html_link:
+        return Team.objects.get(name='Celtics')
+    if 'bucks' in team_html_link:
+        return Team.objects.get(name='Bucks')
+    if 'wizards' in team_html_link:
+        return Team.objects.get(name='Wizards')
+    if 'pacers' in team_html_link:
+        return Team.objects.get(name='Pacers')
+    if 'pistons' in team_html_link:
+        return Team.objects.get(name='Pistons')
+    if 'jazz' in team_html_link:
+        return Team.objects.get(name='Jazz')
+    if 'kings' in team_html_link:
+        return Team.objects.get(name='Kings')
+    if 'suns' in team_html_link:
+        return Team.objects.get(name='Suns')
+    if 'mavericks' in team_html_link:
+        return Team.objects.get(name='Mavericks')
+    if 'hornets' in team_html_link:
+        return Team.objects.get(name='Hornets')
+    if 'magic' in team_html_link:
+        return Team.objects.get(name='Magic')
+    if 'knicks' in team_html_link:
+        return Team.objects.get(name='Knicks')
+    if 'wolves' in team_html_link:
+        return Team.objects.get(name='Timberwolves')
+    if 'nuggets' in team_html_link:
+        return Team.objects.get(name='Nuggets')
+    if 'blazers' in team_html_link:
+        return Team.objects.get(name='Trail Blazers')
+    if 'nets' in team_html_link:
+        return Team.objects.get(name='Nets')
+    if '76ers' in team_html_link:
+        return Team.objects.get(name='76ers')
+    raise ValueError('Could not find team!')
 
--- nba/tests.py
+++ nba/tests.py
@@ -1,36 +1,62 @@
 from django.test import TestCase
 from django.core.management import call_command
 
+from nba.scrapper.week_data import WeekData
+
 # Create your tests here.
 
-class ScrapperTest(TestCase):
+# class ScrapperTest(TestCase):
 
-    def test_initial_data_load(self):
-        call_command('init_teams')
-        return
+    # def test_initial_data_load(self):
+        # call_command('init_teams')
+        # return
 
-    def test_scrape_connection(self):
-        call_command('fetch_week')
-        return
+    # def test_scrape_connection(self):
+        # call_command('fetch_week')
+        # return
 
-    def test_delte_week(self):
-        call_command('delete_week')
+    # def test_delte_week(self):
+        # call_command('delete_week')
 
-    def test_got_full_league(self):
-        return
+    # def test_got_full_league(self):
+        # return
 
-    def test_no_rank_duplicates(self):
-        return
+    # def test_no_rank_duplicates(self):
+        # return
     
-class QueryTest(TestCase):
+# class QueryTest(TestCase):
 
-    def test_get_week(self):
-        return
+    # def test_get_week(self):
+        # return
 
-    def test_get_year(self):
-        return
+    # def test_get_year(self):
+        # return
 
-class ModelTest(TestCase):
+class WeekDataTest(TestCase):
 
-    def get_name(self):
-        return
+    def test_init(self):
+        test = WeekData(2016)
+        self.assertEqual(test.year, 2016)
+
+    def test_verify_length(self):
+        test = WeekData(2016)
+        self.assertFalse(test.verify_length())
+
+    def test_verify_ranks(self):
+        test = WeekData(2016)
+        self.assertFalse(test.verify_ranks())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
