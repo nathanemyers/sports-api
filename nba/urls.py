69733e9d0ec3c350e635109abaac1cb7f8097a59
Nathan Myers <nathanemyers@gmail.com>
2016-09-20 15:07:37 -0500

    updating urls

--- nba/urls.py
+++ nba/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'nbapowerranks'
 urlpatterns = [
-        url(r'^rankings/(?P<year>[0-9]+)/(?P<week>[0-9]+)', views.week_rankings),
-        url(r'^rankings/(?P<year>[0-9]+)', views.year_rankings),
-        url(r'^rankings/info', views.info),
+        url(r'rankings/(?P<year>[0-9]+)/(?P<week>[0-9]+)', views.week_rankings),
+        url(r'rankings/(?P<year>[0-9]+)', views.year_rankings),
+        url(r'rankings/info', views.info),
         ]
