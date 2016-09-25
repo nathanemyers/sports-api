79d0753320da3bb5bcec5d856cca11b220f42ebc
Nathan Myers <nathanemyers@gmail.com>
2016-09-25 14:22:51 -0500

    removing duplicate middleware

--- sports_api/settings.py
+++ sports_api/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 #TODO think about hiding this stuff in config
