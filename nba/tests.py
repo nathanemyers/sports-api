7c31cddb2ecec7cbe9d660005361cbf0957b772d
Nathan Myers <nathanemyers@gmail.com>
2016-09-27 18:04:35 -0500

    fixing tests

--- nba/tests.py
+++ nba/tests.py
@@ -1,33 +1,36 @@
 import json
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 from django.core.management import call_command
+from rest_framework.test import APIRequestFactory
 
 from nba.scrapper.week_data import WeekData
-from .views import week_rankings, year_rankings, info
+from nba.views import info, WeekList, YearList
 
 class ApiTest(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
+        self.factory = APIRequestFactory()
         call_command('loaddata', 'full2016')
 
     def test_week_rankings(self):
+        view = WeekList.as_view()
         request = self.factory.get('/rankings/2016/1')
-        response = week_rankings(request, 2016, 1)
+        response = view(request, year=2016, week=1)
+        response.render()
         rankings = json.loads(response.content)
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(rankings['rankings']['Warriors'], 1)
-        self.assertEquals(rankings['rankings']['Pacers'], 25)
+        self.assertEquals(rankings.__len__(), 30)
 
-    # def test_year_rankings(self):
-        # request = self.factory.get('/rankings/2016')
-        # response = year_rankings(request, 2016)
-        # data = json.loads(response.content)
-        # results = data['results']
+    def test_year_rankings(self):
+        view = YearList.as_view()
+        request = self.factory.get('/rankings/2016')
+        response = view(request, year=2016)
+        response.render()
+        results = json.loads(response.content)
 
-        # self.assertEquals(response.status_code, 200)
-        # self.assertEquals(results.__len__(), 30)
-        # self.assertEquals(results[0].__len__(), 24)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(results.__len__(), 30)
+        self.assertEquals(results[0]['rankings'].__len__(), 24)
 
     def test_most_recent(self):
         request = self.factory.get('/rankings/info')
